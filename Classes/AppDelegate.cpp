3502021fa7b16edd259856158dc6729b9a4c2ca4
wu di <wudi1991@gmail.com>
2015-11-12 13:27:08 +0800

    4

--- Classes/AppDelegate.cpp
+++ Classes/AppDelegate.cpp
@@ -1,6 +1,7 @@
 #include "AppDelegate.h"
 #include "MyUtility.h"
-#include "HelloWorldScene.h"
+#include "GameMenuScene.h"
+#include "GameMainScene.h"
 
 USING_NS_CC;
 
@@ -48,7 +49,7 @@ bool AppDelegate::applicationDidFinishLaunching() {
     register_all_packages();
 
     // create a scene. it's an autorelease object
-    auto scene = HelloWorld::createScene();
+	auto scene = GameMenu::scene();
 
     // run
     director->runWithScene(scene);
